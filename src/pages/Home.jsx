316a6235187ea76036371bf6ad93042e7db006b1
Praveen <napraveen03@gmail.com>
2024-05-24 18:56:36 +0530

    Home Page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,21 +1,27 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import GetUserDetails from '../functions/GetUserDetails';
+
 const Home = () => {
   const { userDetails } = GetUserDetails();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!userDetails) {
+      navigate('/login');
+    }
+  }, [userDetails, navigate]);
+
+  if (!userDetails) {
+    return <p>Loading user details...</p>;
+  }
+
   return (
-    <>
-      <div className="home-home_page">
-        {userDetails ? (
-          <>
-            <div className="home-container">
-              <div className="home-dashboard">{userDetails.username}</div>
-            </div>
-          </>
-        ) : (
-          <p>Loading user details...</p>
-        )}
+    <div className="home-home_page">
+      <div className="home-container">
+        <div className="home-dashboard">{userDetails.username}</div>
       </div>
-    </>
+    </div>
   );
 };
 
